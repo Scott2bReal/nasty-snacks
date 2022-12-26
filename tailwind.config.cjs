0ff236167472fd8be95470f52e147b71d19eb033
Scott Hoecker <hoeckers@gmail.com>
2022-12-25 22:05:48 -0600

    Start working on landing page

---
+++ tailwind.config.cjs
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+  theme: {
+    extend: {
+      backgroundImage: {
+        'snacks-banner': "url('./public/transparent_inverted_snacks.jpg')",
+      },
+    },
+  },
+  plugins: [
+    function ({ addVariant }) {
+      addVariant('child', '& > *')
+      addVariant('child-hover', '& > *:hover')
+    },
+  ],
+}
