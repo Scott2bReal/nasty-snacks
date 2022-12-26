e963d7bf8e4f4411ae62afd9ec1bacd3f3d9163f
Scott Hoecker <hoeckers@gmail.com>
2022-12-26 15:17:15 -0600

    Add preact

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,20 +1,20 @@
-import { defineConfig } from 'astro/config'
+import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-import tailwind from '@astrojs/tailwind'
+import tailwind from '@astrojs/tailwind';
 
 // https://astro.build/config
-import image from '@astrojs/image'
+import image from '@astrojs/image';
+
+// https://astro.build/config
+import preact from "@astrojs/preact";
 
 // https://astro.build/config
 export default defineConfig({
   output: 'server',
-  integrations: [
-    tailwind({
-      config: {
-        applyBaseStyles: false,
-      },
-    }),
-    image(),
-  ],
-})
+  integrations: [tailwind({
+    config: {
+      applyBaseStyles: false
+    }
+  }), image(), preact()]
+});
