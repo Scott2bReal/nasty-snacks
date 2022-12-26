c0703b66b9b7e1c912be7ab2ffdbbc5076d5d7fd
Scott Hoecker <hoeckers@gmail.com>
2022-12-26 01:13:07 -0600

    CSS edits

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,11 +2,7 @@
 module.exports = {
   content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
   theme: {
-    extend: {
-      backgroundImage: {
-        'snacks-banner': "url('./public/transparent_inverted_snacks.jpg')",
-      },
-    },
+    extend: {},
   },
   plugins: [
     function ({ addVariant }) {
