8cf85a91abdc84953747b4389b7c156feb01e1d5
Scott Hoecker <hoeckers@gmail.com>
2022-12-29 06:37:07 -0600

    Add names to form fields

--- src/components/contact-form/ContactForm.tsx
+++ src/components/contact-form/ContactForm.tsx
@@ -35,28 +35,27 @@ export const ContactForm = () => {
 
   return (
     <form
-      action='/success'
-      method='POST'
       name='contact'
       id='contact'
       class='flex flex-col gap-2 flex-grow flex-wrap p-4 items-center md:w-[50vw] w-full mx-auto child:w-full'
       onSubmit={async (e) => {
         e.preventDefault()
-        // const formData = new FormData(e.currentTarget)
-        const formData = {
-          firstName: firstName,
-          lastName: lastName,
-          email: email,
-          subject: subject,
-          message: message,
-        }
+        const formData = new FormData(e.currentTarget)
+        // const formData = {
+        //   firstName: firstName,
+        //   lastName: lastName,
+        //   email: email,
+        //   subject: subject,
+        //   message: message,
+        // }
 
         try {
           setSubmitLoading(true)
           await fetch('/', {
             method: 'POST',
             headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
-            body: encode({ 'form-name': 'contact', ...formData }),
+            // body: encode({ 'form-name': 'contact', ...formData }),
+            body: { ...formData },
           })
           clearForm()
           setSubmitLoading(false)
@@ -72,6 +71,7 @@ export const ContactForm = () => {
           </label>
           <input
             id='firstName'
+            name='firstName'
             value={firstName}
             onChange={(e) => setFirstName(e.currentTarget.value)}
             required
@@ -83,6 +83,7 @@ export const ContactForm = () => {
           </label>
           <input
             id='lastName'
+            name='lastName'
             value={lastName}
             onChange={(e) => setlastName(e.currentTarget.value)}
           />
@@ -94,6 +95,7 @@ export const ContactForm = () => {
       </label>
       <input
         id='email'
+        name='email'
         type='email'
         value={email}
         onChange={(e) => setEmail(e.currentTarget.value)}
@@ -105,6 +107,7 @@ export const ContactForm = () => {
       </label>
       <input
         id='subject'
+        name='subject'
         required
         value={subject}
         onChange={(e) => setSubject(e.currentTarget.value)}
@@ -117,6 +120,7 @@ export const ContactForm = () => {
         id='message'
         rows={4}
         class='text-neutral-900 p-1'
+        name='message'
         value={message}
         onChange={(e) => setMessage(e.currentTarget.value)}
         required
