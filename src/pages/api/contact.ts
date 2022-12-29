670340fa9ba3b765eaf12cb3e335bbe109ac28bf
Scott Hoecker <hoeckers@gmail.com>
2022-12-29 12:37:46 -0600

    Log data sent to /api/contact

--- src/pages/api/contact.ts
+++ src/pages/api/contact.ts
@@ -2,6 +2,7 @@ import type { APIRoute } from "astro"
 
 export const post: APIRoute = async ({ request }) => {
   const formData = await request.formData()
+  console.log(formData.get('name'))
 
   return {
     body: JSON.stringify({
