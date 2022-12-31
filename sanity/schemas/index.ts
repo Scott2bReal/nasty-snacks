55fea7606a044a59582542054937df562ef80c53
Scott Hoecker <hoeckers@gmail.com>
2022-12-30 23:49:08 -0600

    Basic sanity data fetching up and running

--- sanity/schemas/index.ts
+++ sanity/schemas/index.ts
@@ -1,1 +1,3 @@
-export const schemaTypes = []
+import show from "./show";
+
+export const schemaTypes = [show]
---
+++ sanity/schemas/show.ts
@@ -0,0 +1,37 @@
+export default {
+  name: 'show',
+  type: 'document',
+  title: 'Show',
+  fields: [
+    {
+      name: 'venueName',
+      type: 'string',
+      title: 'Venue Name',
+    },
+    {
+      name: 'date',
+      type: 'date',
+      title: 'Date',
+    },
+    {
+      name: 'city',
+      type: 'string',
+      title: 'City',
+    },
+    {
+      name: 'state',
+      type: 'string',
+      title: 'State',
+    },
+    {
+      name: 'ticketLink',
+      type: 'string',
+      title: 'Ticket Link'
+    },
+    {
+      name: 'description',
+      type: 'string',
+      title: 'Description',
+    }
+  ],
+}
