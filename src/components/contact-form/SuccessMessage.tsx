706d48354b0715d2fac8373e77c661805cd8c568
Scott Hoecker <hoeckers@gmail.com>
2022-12-30 19:05:28 -0600

    Correct margin of success message

--- src/components/contact-form/SuccessMessage.tsx
+++ src/components/contact-form/SuccessMessage.tsx
@@ -1,6 +1,6 @@
 export const SuccessMessage = () => {
   return (
-    <div class='bg-gradient-to-br to-purple-700 from-pink-700 tracking-widest mt-4 p-2 rounded-lg shadow-black shadow-md transition duration-150 ease-in-out text-center opacity-50'>
+    <div class='bg-gradient-to-br to-purple-700 from-pink-700 tracking-widest mt-2 p-2 rounded-lg shadow-black shadow-md transition duration-150 ease-in-out text-center opacity-50'>
       Thanks for reaching out!
     </div>
   )
