c38214c14e1a670303ae99dfe64d439279ddf5a9
Scott Hoecker <hoeckers@gmail.com>
2022-12-31 09:51:37 -0600

    Upcoming shows pulls from Sanity

--- astro/src/utils/helpers.ts
+++ astro/src/utils/helpers.ts
@@ -1,3 +1,20 @@
 export function randomNumber(max: number): number {
   return Math.floor(Math.random() * max)
 }
+
+export function generateSanityURL(dataset: string, docType: string) {
+  const sanityProjectId = import.meta.env.SANITY_PROJECT_ID
+  const query = encodeURIComponent(`*[_type == "${docType}"]`)
+
+  return `https://${sanityProjectId}.api.sanity.io/v2021-10-21/data/query/${dataset}?query=${query}`
+}
+
+export function dateFormatter(dateString: string) {
+  const formatter = new Intl.DateTimeFormat('en-US', {
+    weekday: 'long',
+    month: 'long',
+    day: 'numeric',
+  })
+
+  return formatter.format(new Date(`${dateString}T00:00:00-06:00`)).toUpperCase()
+}
