deb29144169b0bf984868a00adbfffbb365353f9
Scott Hoecker <hoeckers@gmail.com>
2023-01-01 00:52:01 -0600

    Add prefetch integration

--- astro/astro.config.mjs
+++ astro/astro.config.mjs
@@ -10,12 +10,15 @@ import preact from "@astrojs/preact";
 import netlify from "@astrojs/netlify/edge-functions";
 
 // https://astro.build/config
+import prefetch from "@astrojs/prefetch";
+
+// https://astro.build/config
 export default defineConfig({
   output: 'server',
   integrations: [tailwind({
     config: {
       applyBaseStyles: false
     }
-  }), preact()],
+  }), preact(), prefetch()],
   adapter: netlify()
 });
