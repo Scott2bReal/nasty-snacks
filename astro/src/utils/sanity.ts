73f5476a55bdbc1b78fbde162ff0d2c99fb378f2
Scott Hoecker <hoeckers@gmail.com>
2023-01-01 12:39:21 -0600

    Band cards slide into view

--- astro/src/utils/sanity.ts
+++ astro/src/utils/sanity.ts
@@ -1,9 +1,13 @@
 export async function sanityAPI(docType: string) {
-  // We'll always need the project ID This will be running on Deno, so we need
-  // a different method to grab env vars in production
+  // We'll always need the project ID. This will be running on Deno in
+  // production, so we need a different method to grab env vars there.
+
+  // Not running Deno locally, and things are working. Waiting on installing as
+  // dependency! Turning off TS just for this line (is working but look here if
+  // things break!)
+
   // @ts-ignore
   const id = import.meta.env.SANITY_PROJECT_ID ?? Deno?.env?.get('SANITY_PROJECT_ID')
-
   const url = generateSanityQueryURL(id, 'production', docType)
 
   return await fetch(url)
