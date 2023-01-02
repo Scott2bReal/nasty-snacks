e39aef5f0714f73d31151ef39aec20a3b8c76163
Scott Hoecker <hoeckers@gmail.com>
2023-01-02 15:51:08 -0600

    Clean up unused code + data

--- astro/src/utils/helpers.ts
+++ astro/src/utils/helpers.ts
@@ -7,12 +7,6 @@ export function randomNumber(max: number): number {
   return Math.floor(Math.random() * max)
 }
 
-export function generateSanityURL(projectId: string, dataset: string, docType: string) {
-  const query = encodeURIComponent(`*[_type == "${docType}"]`)
-
-  return `https://${projectId}.api.sanity.io/v2021-10-21/data/query/${dataset}?query=${query}`
-}
-
 export function dateFormatter(dateString: string) {
   const formatter = new Intl.DateTimeFormat('en-US', {
     weekday: 'long',
