430a860493a254031824469a88adff25c7b7dbe7
Scott Hoecker <hoeckers@gmail.com>
2023-01-07 17:06:13 -0600

    Change top nav styles

--- astro/src/components/navbar/TopNav.tsx
+++ astro/src/components/navbar/TopNav.tsx
@@ -10,30 +10,33 @@ interface Props {
   homeSections: string[]
 }
 
-export const TopNav = ({ mobileControlData, sections, homeSections }: Props) => {
+export const TopNav = ({
+  mobileControlData,
+  sections,
+  homeSections,
+}: Props) => {
   const { lockScroll, mobileOpen, setMobileOpen } = mobileControlData
 
   return (
-    <nav className='text-center bg-neutral-900/90 mx-auto sticky top-0 backdrop-blur-xl z-40'>
-      <div class='flex gap-2 items-center justify-evenly '>
-        <h1 className='p-2 -translate-x-6 md:-translate-x-0'>
-          <a href='/'>
-            <span className='text-gradient text-5xl font-extrabold tracking-widest'>
-              NASTY SNACKS
-            </span>
-          </a>
-        </h1>
-      </div>
-      <ul className='top-0 text-gradient text-center font-extrabold md:flex gap-12 flex-row hidden md:visible items-center justify-center p-3 max-w-[100vw] mx-auto'>
+    <nav className='text-center bg-neutral-900/80 flex justify-between fixed w-full top-0 backdrop-blur-xl z-40'>
+      <h1 className='py-2 pl-4 min-w-[357px]'>
+        <a href='/'>
+          <span className='text-gradient text-4xl font-extrabold tracking-widest'>
+            NASTY SNACKS
+          </span>
+        </a>
+      </h1>
+      <ul className='reverse-text-gradient font-extrabold md:flex gap-12 flex-row hidden lg:visible items-end justify-evenly p-3 max-w-[100vw]'>
         {sections.map((section) => {
           return (
             <li>
               <a
                 rel='prefetch'
-                href={`${homeSections.includes(section)
+                href={`${
+                  homeSections.includes(section)
                     ? `/#${section}`
                     : `/${section}`
-                  }`}
+                }`}
               >
                 {section.toUpperCase()}
               </a>
@@ -41,30 +44,30 @@ export const TopNav = ({ mobileControlData, sections, homeSections }: Props) => 
           )
         })}
       </ul>
-        <button
-          id='openMobileNav'
-          aria-label='Open mobile nav'
-          class='ml-6 md:hidden'
-          onClick={() => {
-            setMobileOpen(true)
-            lockScroll(mobileOpen)
-          }}
+      <button
+        id='openMobileNav'
+        aria-label='Open mobile nav'
+        class='pr-12 md:hidden'
+        onClick={() => {
+          setMobileOpen(true)
+          lockScroll(mobileOpen)
+        }}
+      >
+        <svg
+          className='w-8 h-8'
+          xmlns='http://www.w3.org/2000/svg'
+          fill='none'
+          viewBox='0 0 24 24'
+          stroke-width='1.5'
+          stroke='currentColor'
         >
-          <svg
-            className='w-8 h-8'
-            xmlns='http://www.w3.org/2000/svg'
-            fill='none'
-            viewBox='0 0 24 24'
-            stroke-width='1.5'
-            stroke='currentColor'
-          >
-            <path
-              stroke-linecap='round'
-              stroke-linejoin='round'
-              d='M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5'
-            ></path>
-          </svg>
-        </button>
+          <path
+            stroke-linecap='round'
+            stroke-linejoin='round'
+            d='M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5'
+          ></path>
+        </svg>
+      </button>
     </nav>
   )
 }
