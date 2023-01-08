8619f8212ac8c442432e2b35880b9e027a7ac2c6
Scott Hoecker <hoeckers@gmail.com>
2023-01-07 22:49:47 -0600

    Move music to homepage

--- astro/src/components/navbar/Nav.tsx
+++ astro/src/components/navbar/Nav.tsx
@@ -5,7 +5,7 @@ import { MobileNav } from './MobileNav'
 export const Nav = () => {
   const [mobileOpen, setMobileOpen] = useState(false)
   const sections = ['shows', 'music', 'video', 'band', 'contact']
-  const homeSections = ['shows', 'band', 'video']
+  const homeSections = ['shows', 'band', 'video', 'music']
 
   const lockScroll = (mobileOpen: boolean) => {
     if (mobileOpen) {
