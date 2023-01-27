873af9083ce6dd5187a5ea187962513993bb92e5
Scott Hoecker <hoeckers@gmail.com>
2023-01-27 14:12:02 -0600

    Remove unused components

--- astro/src/components/contact-form/SuccessMessage.tsx
+++
@@ -1,7 +0,0 @@
-export const SuccessMessage = () => {
-  return (
-    <div class='bg-gradient-to-br to-purple-700 from-pink-700 tracking-widest mt-2 -mb-6 p-2 rounded-lg shadow-black shadow-md text-center opacity-50'>
-      Thanks for reaching out!
-    </div>
-  )
-}
