4759ed142d0c47fa02109d496ae4dd47a8cb3a05
Scott Hoecker <hoeckers@gmail.com>
2023-04-03 20:13:59 -0500

    Remove animated socials (for now)

--- astro/astro.config.mjs
+++ astro/astro.config.mjs
@@ -1,31 +1,28 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config"
 
 // https://astro.build/config
-import tailwind from '@astrojs/tailwind';
+import tailwind from "@astrojs/tailwind"
 
 // https://astro.build/config
-import netlify from "@astrojs/netlify/functions";
+import netlify from "@astrojs/netlify/functions"
 
 // https://astro.build/config
-import prefetch from "@astrojs/prefetch";
+import prefetch from "@astrojs/prefetch"
 
 // https://astro.build/config
-import solidJs from "@astrojs/solid-js";
+import solidJs from "@astrojs/solid-js"
 
 // https://astro.build/config
 export default defineConfig({
-  output: 'server',
-  integrations: [tailwind({
-    config: {
-      applyBaseStyles: false
-    }
-  }), prefetch(), solidJs()],
+  output: "server",
+  integrations: [
+    tailwind({
+      config: {
+        applyBaseStyles: false,
+      },
+    }),
+    prefetch(),
+    solidJs(),
+  ],
   adapter: netlify(),
-  vite: {
-    build: {
-      rollupOptions: {
-        external: ['@motionone/solid']
-      }
-    }
-  }
-});
+})
