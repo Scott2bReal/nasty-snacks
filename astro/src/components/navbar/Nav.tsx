e72f630fe6dffd9be9996281c18f8e6c675aa765
Scott Hoecker <hoeckers@gmail.com>
2023-07-26 12:49:22 -0500

    Start adding video hero

--- astro/src/components/navbar/Nav.tsx
+++ astro/src/components/navbar/Nav.tsx
@@ -1,25 +1,42 @@
-import { createSignal, Show } from 'solid-js'
+import { createEffect, createSignal, Show } from "solid-js"
 
 const [isMobileOpen, setIsMobileOpen] = createSignal(false)
-const sections = ['shows', 'music', 'video', 'band', 'contact']
-const homeSections = ['shows', 'band', 'video', 'music']
+const sections = ["shows", "music", "video", "band", "contact"]
+const homeSections = ["shows", "band", "video", "music"]
 
 function TopNav() {
+  const [isScrollTop, setIsScrollTop] = createSignal(true)
+  createEffect(() => {
+    const onScroll = () => {
+      console.log(window.scrollY === 0)
+      setIsScrollTop(window.scrollY === 0)
+      setStyles(isScrollTop() ? "-translate-y-24" : "translate-y-0")
+    }
+    window.addEventListener("scroll", onScroll)
+    return () => window.removeEventListener("scroll", onScroll)
+  }, [isScrollTop()])
+
+  const [styles, setStyles] = createSignal(
+    isScrollTop() ? "-translate-y-24" : "translate-y-0",
+  )
+
   return (
-    <nav class='fixed top-0 left-0 z-40 flex w-[100vw] justify-between bg-neutral-900/20 pr-2 text-center backdrop-blur-xl'>
-      <h1 class='min-w-[357px] py-4'>
-        <a href='/'>
-          <span class='text-gradient text-4xl font-extrabold tracking-widest'>
+    <nav
+      class={`${styles()} fixed left-0 top-0 z-40 flex w-[100vw] justify-between bg-neutral-900/20 pr-2 text-center backdrop-blur-xl transition duration-500 ease-in-out`}
+    >
+      <h1 class="min-w-[357px] py-4">
+        <a href="/">
+          <span class="text-gradient text-4xl font-extrabold tracking-widest">
             NASTY SNACKS
           </span>
         </a>
       </h1>
-      <ul class='reverse-text-gradient hidden max-w-[100vw] flex-row items-end justify-evenly gap-12 p-3 font-extrabold wide:visible wide:flex'>
+      <ul class="reverse-text-gradient hidden max-w-[100vw] flex-row items-end justify-evenly gap-12 p-3 font-extrabold wide:visible wide:flex">
         {sections.map((section) => {
           return (
             <li>
               <a
-                rel='prefetch'
+                rel="prefetch"
                 href={`${homeSections.includes(section)
                     ? `/#${section}`
                     : `/${section}`
@@ -32,25 +49,25 @@ function TopNav() {
         })}
       </ul>
       <button
-        id='openMobileNav'
-        aria-label='Open mobile nav'
-        class='-translate-x-2 pr-12 wide:hidden'
+        id="openMobileNav"
+        aria-label="Open mobile nav"
+        class="-translate-x-2 pr-12 wide:hidden"
         onClick={() => {
           setIsMobileOpen(true)
         }}
       >
         <svg
-          class='h-8 w-8'
-          xmlns='http://www.w3.org/2000/svg'
-          fill='none'
-          viewBox='0 0 24 24'
-          stroke-width='1.5'
-          stroke='currentColor'
+          class="h-8 w-8"
+          xmlns="http://www.w3.org/2000/svg"
+          fill="none"
+          viewBox="0 0 24 24"
+          stroke-width="1.5"
+          stroke="currentColor"
         >
           <path
-            stroke-linecap='round'
-            stroke-linejoin='round'
-            d='M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5'
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5"
           ></path>
         </svg>
       </button>
@@ -61,49 +78,49 @@ function TopNav() {
 function MobileNav() {
   return (
     <nav
-      class={`top-0 bottom-0 z-[5] h-[100vh] w-[100vw] bg-neutral-900 pr-2 text-center`}
+      class={`bottom-0 top-0 z-[5] h-[100vh] w-[100vw] bg-neutral-900 pr-2 text-center`}
     >
-      <div class='flex items-center justify-between gap-2'>
-        <h1 class='min-w-[357px] p-4 opacity-0 md:pl-4'>
-          <a href='/'>
-            <span class='text-gradient text-4xl font-extrabold tracking-widest'>
+      <div class="flex items-center justify-between gap-2">
+        <h1 class="min-w-[357px] p-4 opacity-0 md:pl-4">
+          <a href="/">
+            <span class="text-gradient text-4xl font-extrabold tracking-widest">
               NASTY SNACKS
             </span>
           </a>
         </h1>
         <button
-          class='-translate-x-4 pr-12'
-          id='closeMobileNav'
-          aria-label='Close mobile nav'
+          class="-translate-x-4 pr-12"
+          id="closeMobileNav"
+          aria-label="Close mobile nav"
           onClick={() => {
             setIsMobileOpen(false)
           }}
         >
           <svg
-            class='h-8 w-8'
-            xmlns='http://www.w3.org/2000/svg'
-            fill='none'
-            viewBox='0 0 24 24'
-            stroke-width='1.5'
-            stroke='currentColor'
+            class="h-8 w-8"
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke-width="1.5"
+            stroke="currentColor"
           >
             <path
-              stroke-linecap='round'
-              stroke-linejoin='round'
-              d='M6 18L18 6M6 6l12 12'
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              d="M6 18L18 6M6 6l12 12"
             ></path>
           </svg>
         </button>
       </div>
-      <ul class='text-gradient my-auto mt-6 flex max-w-[100vw] flex-col items-center justify-center gap-12 p-6 text-center font-extrabold lg:flex'>
+      <ul class="text-gradient my-auto mt-6 flex max-w-[100vw] flex-col items-center justify-center gap-12 p-6 text-center font-extrabold lg:flex">
         <li>
           <a
-            href='/'
+            href="/"
             onClick={() => {
               setIsMobileOpen(false)
             }}
           >
-            <span class='text-3xl'>HOME</span>
+            <span class="text-3xl">HOME</span>
           </a>
         </li>
         {sections.map((section) => {
