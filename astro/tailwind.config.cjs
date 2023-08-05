3dd74fcb0717a0a8998d1339b81108457baffc3d
Scott Hoecker <hoeckers@gmail.com>
2023-08-04 22:42:37 -0500

    Add custom easing to animations

--- astro/tailwind.config.cjs
+++ astro/tailwind.config.cjs
@@ -1,20 +1,26 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
   theme: {
     extend: {
+      transitionTimingFunction: {
+        custom: `cubic-bezier(0.83, 0, 0.17, 1)`,
+      },
+      transitionDuration: {
+        2000: "2000ms",
+      },
       backgroundImage: {
-        'bigSnacks': 'url(images/inverted_snacks_big.JPG)'
+        bigSnacks: "url(images/inverted_snacks_big.JPG)",
       },
       screens: {
-        'wide': '890px',
-      }
+        wide: "890px",
+      },
     },
   },
   plugins: [
     function ({ addVariant }) {
-      addVariant('child', '& > *')
-      addVariant('child-hover', '& > *:hover')
+      addVariant("child", "& > *")
+      addVariant("child-hover", "& > *:hover")
     },
   ],
 }
