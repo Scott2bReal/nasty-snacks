b707f9e854a8cc0338583c97129eb814193fb09f
Scott Hoecker <hoeckers@gmail.com>
2023-09-03 13:49:04 -0500

    Add explicit prettier dep, format project

--- astro/src/types/index.d.ts
+++ astro/src/types/index.d.ts
@@ -14,28 +14,27 @@ export type BandMember = {
   picture?: SanityImage
 }
 
-export type SanityImage =
-{
+export type SanityImage = {
+  _type: string
+  asset: {
+    _ref: string
     _type: string
-    asset: {
-      _ref: string
-      _type: string
-    };
-    crop: {
-      _type: string
-      bottom: number
-      left: number
-      right: number
-      top: number
-    };
-    hotspot: {
-      _type: string
-      height: number
-      width: number
-      x: number
-      y: number
-    };
   }
+  crop: {
+    _type: string
+    bottom: number
+    left: number
+    right: number
+    top: number
+  }
+  hotspot: {
+    _type: string
+    height: number
+    width: number
+    x: number
+    y: number
+  }
+}
 
 export type SnacksVid = {
   title: string
--- sanity/schemas/banner.ts
+++ sanity/schemas/banner.ts
@@ -12,6 +12,6 @@ export default {
       name: 'image',
       type: 'image',
       title: 'Image',
-    }
-  ]
+    },
+  ],
 }
