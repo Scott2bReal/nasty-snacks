9a295a94e2886e68f17d5ff4f50bc7ae6aac4300
Scott Hoecker <69652149+Scott2bReal@users.noreply.github.com>
2023-09-03 14:00:25 -0500

    Add explicit prettier dep, format project (#6)

--- sanity/sanity.cli.ts
+++ sanity/sanity.cli.ts
@@ -3,6 +3,6 @@ import {defineCliConfig} from 'sanity/cli'
 export default defineCliConfig({
   api: {
     projectId: '4vlrszzo',
-    dataset: 'production'
-  }
+    dataset: 'production',
+  },
 })
--- sanity/schemas/bandMembers.ts
+++ sanity/schemas/bandMembers.ts
@@ -20,6 +20,6 @@ export default {
       options: {
         hotspot: true,
       },
-    }
+    },
   ],
 }
