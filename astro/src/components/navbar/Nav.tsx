6f43c6c40c51dd393c5e25ddd443d008742790ef
Scott Hoecker <69652149+Scott2bReal@users.noreply.github.com>
2023-09-09 20:50:11 -0500

    Adds socials to mobile nav (#5)
    * Adds socials to mobile nav
    
    * Consolidate shared styles
    
    * Update node version
    
    * Smaller size for mobile nav socials
    
    * Extract footer to component
    
    * Tweak styles
    
    * Formatting
    
    * Remove unused code
    
    * Update prettier config and format project
    
    * Update lockfile

--- astro/src/components/navbar/Nav.tsx
+++ astro/src/components/navbar/Nav.tsx
@@ -1,4 +1,5 @@
 import { createSignal, Show } from "solid-js"
+import SolidSocials from "../socials/SolidSocials"
 
 const [isMobileOpen, setIsMobileOpen] = createSignal(false)
 const sections = ["shows", "music", "video", "band", "contact"]
@@ -126,6 +127,7 @@ function MobileNav() {
           )
         })}
       </ul>
+      <SolidSocials />
     </nav>
   )
 }
