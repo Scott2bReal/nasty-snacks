7370078890114cf62f9ec5494bd09655626ee7a0
Scott Hoecker <hoeckers@gmail.com>
2024-12-20 12:23:35 -0600

    Add promo poster to Sanity

--- sanity/schemas/index.ts
+++ sanity/schemas/index.ts
@@ -2,7 +2,8 @@ import albums from './albums'
 import bandMembers from './bandMembers'
 import bandPhoto from './bandPhoto'
 import banner from './banner'
+import promoPoster from './promo-poster'
 import shows from './shows'
 import videos from './videos'
 
-export const schemaTypes = [shows, bandMembers, videos, albums, banner, bandPhoto]
+export const schemaTypes = [shows, bandMembers, videos, albums, banner, bandPhoto, promoPoster]
---
+++ sanity/schemas/promo-poster.ts
@@ -0,0 +1,17 @@
+export default {
+  name: 'promoPoster',
+  type: 'document',
+  title: 'Promo Poster',
+  fields: [
+    {
+      name: 'alt',
+      type: 'string',
+      title: 'Description',
+    },
+    {
+      name: 'image',
+      type: 'image',
+      title: 'Image',
+    },
+  ],
+}
